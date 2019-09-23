b1fd74974bc55a41581e9c5bcd2ef3eeb95fdde0
root <root@debtest3>
2019-09-23 19:47:38 +0000

    init

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var history = require('connect-history-api-fallback');
+var connect = require('connect');
+
+
+var app = express();
+app.use(history());
+app.locals.title = 'Quantia';
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+module.exports = app;
+
